b04e2d373690656e8eb3deff8643001f37b0c0d8
saohwan <saohwan@codemind.co.kr>
2022-09-29 14:45:20 +0900

    result_json,  반복문 리펙터링.

--- app.py
+++ app.py
@@ -1,53 +1,37 @@
-import os.path
+import json
 
 import fosslight_binary.binary_analysis
 from flask import Flask, jsonify, request
-import requests
-
-from fosslight_binary._jar_analysis import analyze_jar_file
-# from flask import json
-import json
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
 def test_post():
-    file_paths = request.get_json()
-    print(file_paths)
-    a = file_paths["file_path"]
-    print(a)
-    output_file_name = 'test'
-    formatt = 'excel'
+    request_api = request.get_json()
+    binary_dir = request_api["file_path"]
+    format_set = request_api["output_dir"]
+    result = fosslight_binary.binary_analysis.find_binaries(path_to_find_bin=binary_dir, output_dir=binary_dir,
+                                                            format=format_set)
 
-    result = fosslight_binary.binary_analysis.find_binaries(path_to_find_bin=a, output_dir=a,
-                                                            format=formatt)
-    # M = dict(zip(range(len(result) + 1), result))
-    # total = json.dumps(result)
-    # result Tuple => str 변환
+    # result Tuple = > str 변환
     json_str = json.dumps(result)
 
     # result Str => JSON??
     json_list = json.loads(json_str)
 
+    del json_list[0]
+
+    rows = json_list[0]
     # Excel Key 값
-    key = ["Binary Name", "OSS Name", "OSS Version", "License", "Download Location", "Homepage", "Copyright Text",
-           "Exclude", "Comment"]
+    keys = ["Binary Name", "OSS Name", "OSS Version", "License", "Download Location", "Homepage", "Copyright Text",
+            "Exclude", "Comment"]
 
-    # Result 결과 값 넣을 변수 지정
-    result_json = []
+    result_json = [dict(zip(keys, row)) for row in rows]
 
-    # JSON 변환한 Result 값 반복문 실행
-    for arr in json_list[1]:
-        json_arr = {}
-        # Result 결과 값을 하나씩 실행해서 Key, Value JSON 변환
-        for idx, value in enumerate(arr):
-            if len(key) > idx:
-                json_arr[key[idx]] = value
-        # JSON 값 Append
-        result_json.append(json_arr)
+    # del result_json[0]
 
-    return jsonify({"result": result_json})
+    return jsonify(result_json)
 
 
 if __name__ == '__main__':
